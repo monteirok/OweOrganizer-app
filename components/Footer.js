ee31f659cc761c70541d18bae79698654c2b132a
Karsten Monteiro <karstenmonteiro@Karstens-MacBook-Pro.local>
2023-09-19 07:52:32 -0600

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import utilStyles from '../styles/utils.module.css';
+
+function Footer() {
+    return (
+        <footer>
+            <span className={utilStyles.center}>&copy; 2023 kmDev. All rights reserved.</span>
+        </footer>
+    );
+}
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import '../styles/globals.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Main from '../components/Main';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+    return (
+        <div className='appContainer'>
+            <Header />
+            <Main>
+                <Component {...pageProps} />
+            </Main>
+            <Footer />
+        </div>
+    );
 }
+
+export default MyApp;
