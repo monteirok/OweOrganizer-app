d2cf7bf87cc014ade6db0d5568880ce00f4a2523
Karsten Monteiro <karstenmonteiro@Karstens-MacBook-Pro.local>
2023-09-19 18:04:05 -0600

    re-enabled google fonts

--- pages/index.js
+++ pages/index.js
@@ -28,14 +28,12 @@ function Mobile() {
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* fonts */}
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" /> */}
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className={`${utilStyles.center} ${utilStyles.mainHeaderContainer}`}>
-        {/* <h1 className={utilStyles.mainHeader}>MY PROJECTS</h1>
-        <p className={utilStyles.mainSubHeader}>Some personal projects that I've completed for fun/to further develop my developing skills. Enjoy!<span className={utilStyles.smileySymbol}> ◡̈</span></p> */}
         <h1 className={`${utilStyles.mainHeader} ${utilStyles.responsiveHeader}`}>MY PROJECTS</h1>
         <p className={`${utilStyles.mainSubHeader} ${utilStyles.responsiveSubHeader}`}>
           Some personal projects that I've completed for fun/to further develop my developing skills. Enjoy!
