3ef509ea031aafc0b80187cc668a5cc1ea5da21f
Karsten Monteiro <karstenmonteiro@Karstens-MacBook-Pro.local>
2023-09-21 03:30:18 -0600

    updated

--- pages/projects/svg-buttons.js
+++ pages/projects/svg-buttons.js
@@ -10,8 +10,8 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
-      <button className={svgBtnStyles.button-15} role="button">Button 15</button>
-      <button class="button-19" role="button">Button 19</button>
+      <button className={svgBtnStyles.button15} role="button">Button 15</button>
+      <button className={svgBtnStyles.button19} role="button">Button 19</button>
     </>
   );
 }
